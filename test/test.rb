2858a374c6a123758a5a21a8bf4b78bdbf9a5a9f
Daniel Igel <digel@suse.com>
2012-02-20 13:31:42 +0100

    add vcr

--- test/test.rb
+++ test/test.rb
@@ -6,53 +6,29 @@ require '../test/test_helper'
 class TestUpload < Test::Unit::TestCase
 
   def test_upload
+     VCR.use_cassette('example.com', {:match_requests_on => [:method], :record => :new_episodes}) do
+        cloudup = VCloudUpload::Client.new('Max_Mustermann', 'SRo', '123456', "example.com")
 
-      cloudup = VCloudUpload::Client.new('Max_Mustermann', 'SRo', '123456', "example.com")
+        puts 'All vDCs:'
+        i = 0
+        cloudup.each(:vdc) do |vdc|
+          i += 1
+          puts "#{i.to_s}. #{vdc.name}"
+        end
 
-      i = 0
-      cloudup.each('vdc') do |vdc|
-        i += 0
-        puts "#{i.to_s}. #{vdc.name}"
-      end
-      i = gets.chomp.to_i
-
-      j = 0
-      cloudup.each('catalog') do |item|
-        j += 1
-        puts "#{j.to_s}. #{item.link}"
-      end
-      j = gets.chomp.to_i
-
-    cloudup.upload_ovf(cloudup.each('vdc')[i].link, cloudup.each('catalog')[j].link, 'vCloudUpload Test',  'vCloudUpload.i686-0.0.1','/home/user/vCloudUpload-0.0.1', 'Just a little test.', 52000000)
-
-    puts client.status
-
-    cloudup.logout
+        puts 'All catalogs:'
+        j = 0
+        cloudup.each(:catalog) do |item|
+          j += 1
+          puts "#{j.to_s}. #{item.name}"
+        end
 
 
-    # Block version
+      cloudup.upload_ovf(cloudup.each(:vdc)[0].link, cloudup.each(:catalog)[1].link, 'vCloudUpload Test',  'vCloudWrapper.i686-0.0.1','/home/user/vCloudWrapper-0.0.1')
 
-    VCloudUpload.session('Max_Mustermann', 'SRo', '123456', "localhost") do |client|
+      cloudup.logout
 
-      i = 0
-      client.each('vdc') do |vdc|
-        i += 0
-        puts "#{i.to_s}. #{vdc.name}"
-      end
-      i = gets.chomp.to_i
-
-      j = 0
-      client.each('catalog') do |item|
-        j += 1
-        puts "#{j.to_s}. #{item.link}"
-      end
-      j = gets.chomp.to_i
-
-      client.upload_ovf(client.each('vdc')[i].link, client.each('catalog')[j].link, 'vCloudUpload Test',  'vCloudUpload.i686-0.0.1','/home/user/vCloudUpload-0.0.1', 'Just a little test.', 100)
-
-      puts client.status
-    end
-
+     end
 
   end
 
